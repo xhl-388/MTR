2ba2f777b62bc9617512f16573fee43e632bb288
xhl <2682950883@qq.com>
2022-03-28 12:51:40 +0800

    start with a really simple shading model

--- main.cpp
+++ main.cpp
@@ -7,7 +7,7 @@
 const TGAColor white = TGAColor(255, 255, 255, 255);
 const TGAColor red   = TGAColor(255, 0,   0,   255);
 const TGAColor green = TGAColor(0, 255, 0, 255); 
-// Model *model = NULL;
+Model *model = NULL;
 const int width  = 200;
 const int height = 200;
 
@@ -75,35 +75,34 @@ void triangle(Vec2i* pts, TGAImage& image, TGAColor color)
 
 int main(int argc, char** argv) {
 	TGAImage image(width, height, TGAImage::RGB);
-	// if (2==argc) {
-    //     model = new Model(argv[1]);
-    // } else {
-    //     model = new Model("obj/african_head.obj");
-    // }
+	if (2==argc) {
+        model = new Model(argv[1]);
+    } else {
+        model = new Model("obj/african_head.obj");
+    }
 
-    // TGAImage image(width, height, TGAImage::RGB);
-    // for (int i=0; i<model->nfaces(); i++) {
-    //     std::vector<int> face = model->face(i);
-    //     for (int j=0; j<3; j++) {
-    //         Vec3f v0 = model->vert(face[j]);
-    //         Vec3f v1 = model->vert(face[(j+1)%3]);
-    //         int x0 = (v0.x+1.)*width/2.;
-    //         int y0 = (v0.y+1.)*height/2.;
-    //         int x1 = (v1.x+1.)*width/2.;
-    //         int y1 = (v1.y+1.)*height/2.;
-    //         line(x0, y0, x1, y1, image, white);
-    //     }
-    // }
-	Vec2i t0[3] = {Vec2i(10, 70),   Vec2i(50, 160),  Vec2i(70, 80)};
-    Vec2i t1[3] = {Vec2i(180, 50),  Vec2i(150, 1),   Vec2i(70, 180)};
-    Vec2i t2[3] = {Vec2i(180, 150), Vec2i(120, 160), Vec2i(130, 180)};
+	Vec3f light_dir(0,0,-1); // define light_dir
 
-    triangle(t0, image, red);
-    triangle(t1, image, white);
-    triangle(t2, image, green);
+	for (int i=0; i<model->nfaces(); i++) { 
+		std::vector<int> face = model->face(i); 
+		Vec2i screen_coords[3]; 
+		Vec3f world_coords[3]; 
+		for (int j=0; j<3; j++) { 
+			Vec3f v = model->vert(face[j]); 
+			screen_coords[j] = Vec2i((v.x+1.)*width/2., (v.y+1.)*height/2.); 
+			world_coords[j]  = v; 
+		} 
+		Vec3f n = (world_coords[2]-world_coords[0])^(world_coords[1]-world_coords[0]); 
+		n.normalize(); 
+		float intensity = n*light_dir; 
+		if (intensity>0) { 
+			triangle(screen_coords, image, TGAColor(intensity*255, intensity*255, intensity*255, 255)); 
+		} 
+	}
+
     image.flip_vertically(); // i want to have the origin at the left bottom corner of the image
-    image.write_tga_file("output.tga");
-    // delete model;
+    image.write_tga_file("output1.tga");
+    delete model;
     return 0;
 }
 
